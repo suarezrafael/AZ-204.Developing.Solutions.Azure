b27ae98779a6d82730f8bc75bce57769021adbd1
Rafael Vieira Suarez <rafaelv_s@hotmail.com>
2024-03-13 23:07:04 -0300

    Adicionada pastas de projetos azure functions e web apps

---
+++ Azure Functions/Calling an Azure Function from a web app/sqlapp/sqlapp/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using sqlapp.Models;
+using System.Data.SqlClient;
+using System.Text.Json;
+
+namespace sqlapp.Services
+{
+
+    // This service will interact with our Product data in the SQL database
+    public class ProductService : IProductService
+    {
+
+        public async Task <List<Product>> GetProducts()
+        {
+            String FunctionURL = "https://newapp200030.azurewebsites.net/api/GetProducts?code=RVVlGJpJ4paRmO6z6GStMbNZPPMeUDaHg6UzfFi/xrOMSEDMRKzNLg==";
+
+            using (HttpClient _client = new HttpClient())
+            {
+                HttpResponseMessage _response = await _client.GetAsync(FunctionURL);
+                string _content = await _response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<Product>>(_content);
+            }
+
+            }
+    }
+}
+
